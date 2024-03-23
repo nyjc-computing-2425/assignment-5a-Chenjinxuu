107ac75135cedb30dff1b14c1c3818a9fe9f3830
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-23 12:06:10 +0000

    semi-submission
    there is some problems with my code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def to_hms(seconds: int) -> list:
+def to_hms(seconds: int): #-> list:
     """
     Converts seconds to hours, minutes, and seconds, and returns it as a list.
 
@@ -6,7 +6,7 @@ def to_hms(seconds: int) -> list:
     ---------
     seconds: int
         the seconds to be calculated
-
+        
     Returns
     ---------
     list
@@ -19,7 +19,21 @@ def to_hms(seconds: int) -> list:
     [0, 1, 1]
     >>> to_hms(7199)
     [1, 59, 59]
+    >>> to_hms(99.99)
+    Unsupported input type.
     """
-    # Type your code below
-    pass
-
+    # Type your code below 
+   
+    
+    minutes = 0 
+    hours = 0
+  
+    if "." not in str(seconds) and str(seconds).isdigit() and int(seconds) > 0 and not type(seconds) == str:
+        minutes , seconds = divmod(int(seconds), 60)
+        hours , minutes = divmod(int(minutes), 60)
+        hours = int(hours)
+        result = [int(hours), int(minutes), int(seconds)]
+        return print(result)
+        
+    else:
+        return print("Unsupported input type.")
